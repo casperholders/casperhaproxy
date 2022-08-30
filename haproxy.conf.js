5336ca477e25ea2b0ec4a45cd6fa15bc7f6e24bc
Killian Hascoet <killianh@live.fr>
2022-08-30 21:46:07 +0200

    Add testnet / mainnet node monitor

--- haproxy.conf.js
+++ haproxy.conf.js
@@ -16,14 +16,33 @@ frontend stats
 \tstats enable
 \tstats uri /stats
 
+listen mainnet_metrics
+\tbind *:8888
+\tbalance roundrobin
+\thttp-response set-header Access-Control-Allow-Origin "http://localhost:8080"
+\thttp-response set-header Access-Control-Allow-Methods "GET, POST, OPTIONS"
+\thttp-response set-header Access-Control-Allow-Headers "Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding"
+\tserver www1 116.202.217.126:8888 maxconn 50 check inter 10s
+
+listen testnet_metrics
+\tbind *:8889
+\tbalance roundrobin
+\thttp-response set-header Access-Control-Allow-Origin "http://localhost:8080"
+\thttp-response set-header Access-Control-Allow-Methods "GET, POST, OPTIONS"
+\thttp-response set-header Access-Control-Allow-Headers "Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding"
+\tserver www1 95.216.44.9:8888 maxconn 50 check inter 10s
+
 listen www
 \tbind *:${port}
 \tbalance roundrobin
 \thttp-response set-header Access-Control-Allow-Origin "http://localhost:8080"
 \thttp-response set-header Access-Control-Allow-Methods "GET, POST, OPTIONS"
-\thttp-response set-header Access-Control-Allow-Headers "Origin, X-Requested-With, Content-Type, Accept"
+\thttp-response set-header Access-Control-Allow-Headers "Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding"
 \toption httpchk
 \thttp-check send meth POST  uri /rpc  hdr Content-Type application/json  body "{ \\"jsonrpc\\": \\"2.0\\", \\"id\\": 1,    \\"method\\": \\"info_get_status\\",    \\"params\\": []}"
 \thttp-check expect string ${chainspec_name}
+\tcompression algo gzip
+\tcompression type text/html text/plain text/xml text/css text/javascript application/javascript application/json text/json
+\tlog stdout  format raw  local0  info
 ${backends}`
 }
